43dc131ce1d3ca490c36e402c9bf0a8060452947
Mdhlimon <mdhlimonmia@gmail.com>
2023-03-04 15:14:19 +0600

    Card Design ok

--- js/getFile.js
+++ js/getFile.js
@@ -7,20 +7,49 @@ const loadData = async() =>{
 const displayData = (hubs) =>{
 
     const dataContainer = document.getElementById('hubs-container');
-    console.log(hubs);
+    
     hubs.forEach(hub =>{
         const hubDiv = document.createElement('div');
         hubDiv.classList.add('col');
         hubDiv.innerHTML = `
-        <div class="card">
-        <div class="card">
-            <img src="${hub.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title"></h5>
-            <p class="card-text"></p>
+        <div class="card p-3" style="height: 450px">
+            <img style="height: 200px" src="${hub.image}" class="card-img-top" alt="...">
+            <div class="card-body p-0">
+            <h5 class="card-title">Features</h5>
+            <ol id = "${hub.id}">
+            ${hub.features[0]?  ` <li> ${hub.features[0]} </li>`: ''}
+            ${hub.features[1]?  ` <li> ${hub.features[1]} </li>`: ''}
+            ${hub.features[2]?  ` <li> ${hub.features[2]} </li>`: ''}
+            ${hub.features[3]?  ` <li> ${hub.features[3]} </li>`: ''}
+            </ol>            
+           <hr>
+           <div class="d-flex justify-content-between align-items-center" >
+             <div>
+                <h4 class="fs-5 ">${hub.name}</h4> 
+                <p>${hub.published_in}</p>
             </div>
+            <div>
+                <button>button</button>
+            </div>
+            </div>
+        </div>
         `;
-        console.log(hubDiv);
+/*
+        const iD = parseInt(hub.id);
+        console.log(typeof(iD));
+        const featuresLine = document.getElementById('01');
+         const newLine = document.createElement('li');
+         newLine.innerText = `${hub.features[1]}`
+        //  featuresLine.appendChild(newLine);
+        //  console.log(newLine.innerText);
+       
+        hub.features.forEach(line => {
+            newLine.innerHTML =`
+            <li>${line}</li>
+            ` 
+            });
+        //     featuresLine.appendChild(newLine);
+        // console.log(featuresLine);  */
         dataContainer.appendChild(hubDiv);
     });
 }
